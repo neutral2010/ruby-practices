00c6446a352a6e59435ff0f1a47cecdad0298fd0
neutral2010 <kasiwamoti5@gmail.com>
2022-09-06 20:36:27 +0900

    仮

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -5,17 +5,29 @@ require_relative 'shot'
 
 class Game
   def initialize(score_texts)
-    @frames = []
-      scores = score_texts.split(',')
-      score_numbers = []
-      scores.each do |score_text|
-        if score_text == 'X'
-          score_numbers << 10
-        else
-          score_numbers << score_text.to_i
-        end
+    scores = score_texts.split(',')
+    score_numbers = []
+    scores.each do |score_text|
+      if score_text == 'X'
+        score_numbers << 10
+      else
+        score_numbers << score_text.to_i
       end
-    p score_numbers
+    end
+    # スコアナンバーズを先頭から2個ずつ10個の配列に分ける。
+    # もし、10の場合は1つでよい。9つできたら、残りはひとまとめでよい。
+    @frames = []
+    frame = []
+    score_numbers.each do |score_number|
+      frame << score_number
+      if frame.size == 2
+        frame = []
+      end
+      p "frame: #{frame}"
+      p frame.object_id
+      @frames << frame
+      p @frames
+    end
     # frame_marks = score_numbers.each_slice(2).to_a
     # frame_marks.each_index do |index|
     #   @frames << Frame.new(frame_marks[index][0], frame_marks[index][1], frame_marks, index)
