b39d620e94950446326bba78a3d4d1ac8ef74ac0
neutral2010 <kasiwamoti5@gmail.com>
2022-09-13 06:36:56 +0900

    Revert "質問するためのコミット"
    This reverts commit 9b45209f157441b2bac70ae052209e84aadb2024.

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# require_relative 'game'
-
 class Frame
   attr_accessor :frame
 
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -3,44 +3,19 @@
 require_relative 'frame'
 
 class Game
-  attr_accessor :frames
-
   def initialize(score_texts)
-    score_numbers = make_score_numbers(score_texts)
-    make_frames(score_numbers)
-    @frames.each do |frame|
-      Frame.new(frame)
-    end
-  end
-
-  def calc_total_score
-    total_score = []
-    # @frames.each_with_index do |f, i|
-    @frames.each_with_index do |frame, i|
-      total_score <<
-        # スペアだったら
-        # if f.sum == 10 && f.size == 2
-        if rolls.spare?
-          # スペアの時の計算メソッド
-          frame.calc_frame + @frames[i + 1][0]
-          # f.sum + @frames[i + 1][0]
-        # ストライクだったら
-          # ストライクの時の計算メソッド
-        else
-          frame.calc_frame
-          # f.sum
-        end
-    end
-    total_score
-  end
-
-  def make_score_numbers(score_texts)
     score_numbers = []
     score_texts.split(',').each do |score_text|
-      # score_text == 'X' ? score_numbers << 10 : score_numbers << score_text.to_i
-      score_numbers << (score_text == 'X' ? 10 : score_text.to_i)
+      score_numbers << if score_text == 'X'
+                         10
+                       else
+                         score_text.to_i
+                       end
     end
-    score_numbers
+    make_frames(score_numbers)
+    @frames.each do |frame|
+      p Frame.new(frame)
+    end
   end
 
   def make_frames(score_numbers)
@@ -48,11 +23,11 @@ class Game
     frame = []
     score_numbers.each do |score_number|
       frame << score_number
-      # p "frame:#{frame}"
+      p "frame:#{frame}"
       if @frames.size < 9 && frame.size == 2
         @frames << frame
         frame = []
-      elsif frames.size < 9 && score_number == 10
+      elsif @frames.size < 9 && score_number == 10
         frame.size == 1
         @frames << frame
         frame = []
@@ -63,6 +38,5 @@ class Game
         @frames << frame
       end
     end
-    p @frames
   end
 end
