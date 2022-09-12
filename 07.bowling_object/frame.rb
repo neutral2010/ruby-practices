9b45209f157441b2bac70ae052209e84aadb2024
neutral2010 <kasiwamoti5@gmail.com>
2022-09-13 05:35:40 +0900

    質問するためのコミット
    「Frameクラスのメソッドが使えない。」を解決したい。

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# require_relative 'game'
+
 class Frame
   attr_accessor :frame
 
--- 07.bowling_object/main.rb
+++ 07.bowling_object/main.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 require_relative 'game'
-
-# def score_numbers_for_frame(score_texts)
-#   scores = score_texts.split(',')
-#   score_numbers = []
-#   scores.each do |score_text|
-#     if score_text == 'X'
-#       score_numbers << 10
-#     else
-#       score_numbers << score_text.to_i
-#     end
-#   end
-#   score_numbers
-# end
+require_relative 'frame'
 
 if __FILE__ == $PROGRAM_NAME
   score_texts = ARGV[0]
   game = Game.new(score_texts)
+  p game.calc_total_score
 end
